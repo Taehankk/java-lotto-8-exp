ece1b47b447bc38a5b247de7cd2b06154bafaceb
KTH <1514kth@naver.com>
2025-11-08 19:52:17 +0900

    Spring Boot 프로젝트 초기 세팅

---
+++ spring-lotto/src/main/java/com/lotto/gamble/SpringLottoApplication.java
@@ -0,0 +1,13 @@
+package com.lotto.gamble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLottoApplication.class, args);
+	}
+
+}
---
+++ spring-lotto/src/test/java/com/lotto/gamble/demo/SpringLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotto.gamble.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
