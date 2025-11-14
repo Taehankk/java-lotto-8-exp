07086000316883aa90aaea0e41b64ab16fc34f3f
KTH <1514kth@naver.com>
2025-11-14 15:35:44 +0900

    로또 당첨 계산기 관련 파일 생성

---
+++ spring-lotto/src/main/java/com/lotto/gamble/controller/LottoCalController.java
@@ -0,0 +1,10 @@
+package com.lotto.gamble.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lotto/cal")
+public class LottoCalController {
+
+}
---
+++ spring-lotto/src/main/java/com/lotto/gamble/service/LottoCalService.java
@@ -0,0 +1,8 @@
+package com.lotto.gamble.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoCalService {
+
+}
